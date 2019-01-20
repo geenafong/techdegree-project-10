3494b8dbbe2221722e6360bd4f81d6ec37057ccd
geenafong <geenafong@gmail.com>
2019-01-20 15:39:58 -0800

    private route

--- client/src/App.js
+++ client/src/App.js
@@ -111,7 +111,7 @@ class App extends Component {
                         <Route exact path='/signup' component={UserSignUp} />
                         <Route exact path='/courses/create' render= {() => <CreateCourse user={user}/>}/>
                         <Route exact path='/courses/:id' render={ ({match}) => <CourseDetails id={match.params.id} />}/>
-                        <PrivateRoute exact path='/courses/:id/update' render={ ({match}) => <UpdateCourse id={match.params.id} />}/>                       
+                        <PrivateRoute exact path='/courses/:id/update' component = {UpdateCourse}/>                       
                         <Route exact path='/signout' render={() => <UserSignOut signOut={this.signOut} /> } />
                     </Switch>
                     </div>
